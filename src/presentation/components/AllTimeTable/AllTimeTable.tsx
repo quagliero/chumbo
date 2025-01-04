c237da13cdc4b4fe811947846648b78d4058fd0a
quag <4254570+quagliero@users.noreply.github.com>
2025-01-04 09:33:42 +0000

    table sort

--- src/presentation/components/AllTimeTable/AllTimeTable.tsx
+++ src/presentation/components/AllTimeTable/AllTimeTable.tsx
@@ -16,7 +16,9 @@ interface TeamStats {
   ties: number;
   winPerc: number;
   points_for: number;
+  points_for_avg: number;
   points_against: number;
+  points_against_avg: number;
 }
 
 const teamStats: Record<string, TeamStats> = {};
@@ -36,8 +38,10 @@ Object.entries(seasons).forEach(([, season]) => {
         ties: settings.ties,
         winPerc: 0,
         points_for: settings.fpts + settings.fpts_decimal / 100,
+        points_for_avg: 0,
         points_against:
           settings.fpts_against + settings.fpts_against_decimal / 100,
+        points_against_avg: 0,
       };
     } else {
       teamStats[owner_id].team_name = user.metadata.team_name;
@@ -56,6 +60,8 @@ const sortedStats = Object.values(teamStats)
   .map((t) => ({
     ...t,
     winPerc: (t.wins / (t.wins + t.losses + t.ties)) * 100,
+    points_for_avg: t.points_for / (t.wins + t.losses + t.ties),
+    points_against_avg: t.points_against / (t.wins + t.losses + t.ties),
   }))
   .sort((a, b) => {
     if (a.winPerc !== b.winPerc) return b.winPerc - a.winPerc;
@@ -72,26 +78,29 @@ const AllTimeTable = () => {
   const columns = [
     columnHelper.accessor("team_name", {
       cell: (info) => <div className="text-left">{info.getValue()}</div>,
-      header: () => <div className="text-left">Team</div>,
-      enableSorting: true,
-      sortingFn: "alphanumeric",
+      header: () => <span className="mr-auto">Team</span>,
+      enableSorting: false,
     }),
     columnHelper.accessor("wins", {
+      header: () => "Wins",
       cell: (info) => number(info.getValue()),
       sortingFn: "alphanumeric",
       enableSorting: true,
     }),
     columnHelper.accessor("losses", {
+      header: () => "Losses",
       cell: (info) => number(info.getValue()),
       sortingFn: "alphanumeric",
       enableSorting: true,
     }),
     columnHelper.accessor("ties", {
+      header: () => "Ties",
       cell: (info) => number(info.getValue()),
       sortingFn: "alphanumeric",
       enableSorting: true,
     }),
     columnHelper.accessor("winPerc", {
+      header: () => "Win %",
       cell: (info) =>
         number(info.getValue(), {
           maximumFractionDigits: 2,
@@ -100,12 +109,26 @@ const AllTimeTable = () => {
       enableSorting: true,
     }),
     columnHelper.accessor("points_for", {
+      header: () => "For",
       cell: (info) => number(info.getValue()),
       sortingFn: "alphanumeric",
       enableSorting: true,
     }),
+    columnHelper.accessor("points_for_avg", {
+      header: () => "Avg.",
+      cell: (info) => number(info.getValue(), { maximumFractionDigits: 2 }),
+      sortingFn: "alphanumeric",
+      enableSorting: true,
+    }),
     columnHelper.accessor("points_against", {
+      header: () => "Against",
       cell: (info) => number(info.getValue()),
+      sortingFn: "alphanumeric",
+      enableSorting: true,
+    }),
+    columnHelper.accessor("points_against_avg", {
+      header: () => "Avg.",
+      cell: (info) => number(info.getValue(), { maximumFractionDigits: 2 }),
       sortingFn: "alphanumeric",
       enableSorting: true,
     }),
@@ -121,30 +144,43 @@ const AllTimeTable = () => {
   return (
     <div className="container mx-auto">
       <table className="w-full text-right">
-        <thead>
+        <thead className="text-sm font-medium">
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
                 <th
                   key={header.id}
                   onClick={header.column.getToggleSortingHandler()}
+                  className={
+                    header.column.getCanSort()
+                      ? "cursor-pointer select-none"
+                      : ""
+                  }
                 >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
+                  <span className="flex justify-end">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    {header.column.getCanSort()
+                      ? {
+                          asc: " 🔼",
+                          desc: " 🔽",
+                        }[header.column.getIsSorted() as string] ?? " ↕️"
+                      : ""}
+                  </span>
                 </th>
               ))}
             </tr>
           ))}
         </thead>
-        <tbody>
+        <tbody className="text-xs divide-y divide-gray-100">
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
+                <td key={cell.id} className="py-2">
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
